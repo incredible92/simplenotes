9f15fc0478da641ab5c04f4b100b7e6e95dd8b35
incredible92 <sulymanabdullahi@gmail.com>
2020-12-06 12:15:49 +0100

    feat: searchBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import SearchBar from './components/searchBar.jsx';
+
+ 
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <SearchBar />
+    
     </div>
   );
 }
---
+++ src/components/searchBar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+// const logos = [
+//   {
+//     image: "/public/icons/icons8-menu-24.png",
+//   },
+
+//   {
+//     image: "/public/icons/icons8-add-property-50.png",
+//   },
+// ];
+
+function searchBar() {
+  return (
+    <div className="searchBar">
+      <div className="menu">
+        <img src="/public/icons/icons8-menu-24.png" alt="icon" />
+      </div>
+
+      <div className="serach-input">
+        <input
+          type="search"
+          name="search"
+          placeholder="All Notes"
+          className="input-field"
+        />
+      </div>
+
+      <div className="menu">
+        <img src="/public/icons/icons8-add-property-50.png" alt="icon" />
+      </div>
+
+      {/* <div className="menu">
+        {logos.map((item, itemIdx) => (
+          <div key={itemIdx} className="icons">
+            <img src={item.image} alt="image" className="images" />
+          </div>
+        ))}
+      </div> */}
+    </div>
+  );
+}
+
+export default searchBar;
