47b4ba1f76c78591ff5baab28464b680c185ec46
incredible92 <sulymanabdullahi@gmail.com>
2020-12-08 10:46:27 +0100

    feat: firenote-config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxVd4Yl2phU5OxlhIl_4P_1xPDaoLUxtU",
+    authDomain: "firenotes-ba626.firebaseapp.com",
+    projectId: "firenotes-ba626",
+    storageBucket: "firenotes-ba626.appspot.com",
+    messagingSenderId: "265859043526",
+    appId: "1:265859043526:web:74f56ff8bb0c6f977347c7",
+    measurementId: "G-FQ82521BRG"
+  };
